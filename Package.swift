5337a14aa97ed5d960d956e33593a027400376b2
MageSantana <33928559+MageSantana@users.noreply.github.com>
2026-02-04 09:25:01 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,13 +19,13 @@ let package = Package(
         ), 
         .package(
             url: "https://github.com/lyra/SnapKit.git",
-            exact: "1.0.0"
+            exact: "1.0.1"
         )
     ],
     targets: [
         .binaryTarget(
             name: "LyraPaymentSDKBinary",
-            url: "https://raw.githubusercontent.com/lyra/ios-sdk/2.8.8/LyraPaymentSDK.xcframework.zip",
+            url: "https://raw.githubusercontent.com/lyra/ios-sdk/2.8.9/LyraPaymentSDK.xcframework.zip",
             checksum: "b05ea94b21671e7156cefa0702a3e332050fcacb54fe45bbb633bf7aeb74483e"
         ),
         .target(
